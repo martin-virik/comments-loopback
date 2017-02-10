b65c56e77a8ca298dbff98ab3c8137c2eea56ab5
Martin Virik <MVirik@sk.ibm.com>
2017-02-10 11:12:43 +0100

    adding a remote method

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,4 +2,14 @@
 
 module.exports = function(Comment) {
 
+    Comment.foo = function(msg, cb) {
+
+        cb(null, msg + ' bar')
+    }
+
+    Comment.remoteMethod('foo', {
+          http: {verb: 'get'},
+          accepts: {arg: 'msg', type: 'string'},
+          returns: {arg: 'greeting', type: 'string'}
+    });
 };
